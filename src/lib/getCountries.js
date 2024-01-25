56608e41bf883c12a698ce3af9426c3d4997bebd
r-happy <hr325913@gmail.com>
2024-01-25 21:58:55 +0900

    fix get Country

--- src/lib/getCountries.js
+++ src/lib/getCountries.js
@@ -7,19 +7,23 @@ export const getCountries = async (limit = 10, offset = 0) => {
     endpoint: `map`,
     queries: {
       offset,
-      limit
+      limit,
     },
     customRequestInit: {
       next: {
-        revalidate: 120
-      }
-    }
-  })
+        revalidate: 120,
+      },
+    },
+  });
 
   if (data.offset + data.limit < data.totalCount) {
-    const contents = await getCountries(data.limit, data.offset + data.limit)
-    return [...data.contents, ...contents]
+    const contents = await getCountries(data.limit, data.offset + data.limit);
+    return [...data.contents, ...contents].sort((a, b) =>
+      a.country_name_jp.localeCompare(b.country_name_jp)
+    );
   }
 
-  return data.contents
-}
+  return data.contents.sort((a, b) =>
+    a.country_name_jp.localeCompare(b.country_name_jp)
+  );
+};
