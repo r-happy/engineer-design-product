80bb16f8dbcdbecfb0906a8a7146ebb4a816b2d4
r-happy <hr325913@gmail.com>
2024-01-25 22:23:51 +0900

    fix analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -33,8 +33,8 @@ export default function RootLayout({
       >
         <Header />
         {children}
-        <Footer />
         <Analytics />
+        <Footer />
       </body>
     </html>
   );
