6585ce4eef43b0e71397d2332abb4f623b6121c2
Rei-hszm <106812882+r-happy@users.noreply.github.com>
2024-02-13 10:59:49 +0900

    Change/page (#40)
    * add video
    
    * fix/styles

--- src/app/making/page.css.ts
+++ src/app/making/page.css.ts
@@ -1,10 +1,18 @@
 import { Colors } from "@/styles/colors";
+import { mediaQueryScreen } from "@/styles/queries";
 import { globalStyle, style } from "@vanilla-extract/css";
 
 export const about_body = style({});
 
 globalStyle(`${about_body} h1, h2`, {
   borderBottom: `solid 1px ${Colors.color.main_border.light}`,
+  marginTop: "0.8rem",
+
+  "@media": {
+    [mediaQueryScreen("md")]: {
+        marginTop: "1.0rem"
+    }
+  }
 });
 
 export const image_wrapper = style({
